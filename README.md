aca774516f645460e561f97500fd54bae576e87e
Erik Pukinskis <erik@snowedin.net>
2025-04-23 14:01:33 -0700

    Added a textarea

--- README.md
+++ README.md
@@ -4,3 +4,9 @@
 yarn install
 yarn start:app:dev
 ```
+
+## Work log
+
+### Wed Apr 23
+
+- Got a new app scaffolded here in React. Looking forward to adding a [Convex](https://docs.convex.dev/quickstart/react) backend.
--- app/App.tsx
+++ app/App.tsx
@@ -1,5 +1,55 @@
-"use client"
+import { styled, globalCss } from "@stitches/react"
+import { useState } from "react"
+
+const globalStyles = globalCss({
+  body: {
+    margin: 20,
+    fontFamily: "sans-serif",
+  },
+})
+
+const Readable = styled("div", {
+  maxWidth: 768,
+  margin: "0 auto",
+})
+
+const Textarea = styled("textarea", {
+  resize: "none",
+  width: "100%",
+  paddingBlock: 15,
+  paddingInline: 20,
+  fontFamily: "inherit",
+  fontSize: "inherit",
+  boxSizing: "border-box",
+  borderRadius: 20,
+  border: "1px solid #888",
+})
 
 export default function App() {
-  return <div>Hello World</div>
+  globalStyles()
+  const [message, setMessage] = useState(
+    "Good morning! Not sure what I should be doing."
+  )
+  const [placeholder, setPlaceholder] = useState("How's it going?")
+
+  const handleKeyPress = (event: React.KeyboardEvent<HTMLTextAreaElement>) => {
+    if (event.key === "Enter" && !event.shiftKey) {
+      event.preventDefault()
+      setMessage("")
+      setPlaceholder("")
+    }
+  }
+
+  return (
+    <Readable>
+      <Textarea
+        placeholder={placeholder}
+        autoFocus
+        value={message}
+        onChange={(event) => setMessage(event.target.value)}
+        onKeyDown={handleKeyPress}
+        onBlur={() => setPlaceholder("How's it going?")}
+      />
+    </Readable>
+  )
 }
